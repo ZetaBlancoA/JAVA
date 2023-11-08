ab23bebf35601c76f1fcbeddf7bedf24bbf3b13d
zeta <zabdiel.blanco03@gmail.com>
2023-11-08 10:27:58 -0500

    initial commit

---
+++ src/main/java/com/bosonit/training/HelloWorldApp.java
@@ -0,0 +1,13 @@
+package com.bosonit.training;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class HelloWorldApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!!!");
+
+    }
+}
